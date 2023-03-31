a93c774308d226c1f621d62cac9af607a2d8fd18
mzrjunaid <mzrjunaid@gmail.com>
2023-04-01 03:26:33 +0500

    My First React Native Application on GitHub

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Job Portal Muzaffargarh</Text>
+      <Text style={styles.text}>Job Portal Muzaffargarh!!</Text>
       <StatusBar style="auto" />
     </View>
   );
